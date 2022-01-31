217130d26f056d3360a02e41f0d96b49040a79af
arkady <arkadiomanucci@gmail.com>
2022-01-31 23:39:39 +0300

    version1

---
+++ lib/main.dart
@@ -0,0 +1,273 @@
+// ignore_for_file: prefer_const_constructors
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BodyPart? defendingBodyPart;
+  BodyPart? attackingBodyPart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(213, 222, 240, 1),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Expanded(child: Center(child: Column(
+                children: [
+                  Text("You"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                ],
+              ))),
+              SizedBox(width: 12),
+              Expanded(child: Center(child: Column(
+                children: [
+                  Text("Enemy"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                  Text("1"),
+                ],
+              ))),
+              SizedBox(width: 16),
+            ],
+          ),
+          Expanded(child: SizedBox()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Row(
+              //   children: [
+              //     Padding(padding: EdgeInsets.only(bottom: 59)),
+              //     SizedBox(width: 31, height: 10),
+              //     //Padding(padding: EdgeInsets.only(bottom: 140)),
+              //     SizedBox(child: Text("2")),
+              //     SizedBox(width: 31, height: 10),
+              //     //Padding(padding: EdgeInsets.only(top: 510, right: 211)),
+              //   ],
+              // ),
+            ],
+          ),
+          Expanded(child: SizedBox()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text("Defend".toUpperCase()),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.head,
+                      selected: defendingBodyPart == BodyPart.head,
+                      bodyPartSetter: _selectDefendingBodyPart,
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.torso,
+                      selected: defendingBodyPart == BodyPart.torso,
+                      bodyPartSetter: _selectDefendingBodyPart,
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.legs,
+                      selected: defendingBodyPart == BodyPart.legs,
+                      bodyPartSetter: _selectDefendingBodyPart,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text("Attack".toUpperCase()),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.head,
+                      selected: attackingBodyPart == BodyPart.head,
+                      bodyPartSetter: _selectAttackingBodyPart,
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.torso,
+                      selected: attackingBodyPart == BodyPart.torso,
+                      bodyPartSetter: _selectAttackingBodyPart,
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    BodyPartButton(
+                      bodyPart: BodyPart.legs,
+                      selected: attackingBodyPart == BodyPart.legs,
+                      bodyPartSetter: _selectAttackingBodyPart,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+            ],
+          ),
+          SizedBox(height: 14),
+          Row(
+            children: [
+              SizedBox(
+                width: 16,
+              ),
+              // GestureDetector(
+              //   onTap: () => BodyPart,
+              //   child: ColoredBox(
+              //     color: _getButtonGoColor(BodyPart),
+
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    if (attackingBodyPart != null &&
+                        defendingBodyPart != null) {
+                      setState(() {
+                        attackingBodyPart = null;
+                        defendingBodyPart = null;
+                      });
+                    }
+                  },
+                  child: SizedBox(
+                    height: 40,
+                    child: ColoredBox(
+                      color:
+                          attackingBodyPart == null || defendingBodyPart == null
+                              ? Colors.black38
+                              : Color.fromRGBO(0, 0, 0, 0.87),
+                      // GO подложка
+                      child: Center(
+                        child: Text(
+                          "Go".toUpperCase(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 16,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //),
+              // ),
+
+              SizedBox(
+                width: 16,
+              )
+            ],
+          ),
+          SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+
+  void _selectDefendingBodyPart(final BodyPart value) {
+    setState(() {
+      defendingBodyPart = value;
+    });
+  }
+
+  void _selectAttackingBodyPart(final BodyPart value) {
+    setState(() {
+      attackingBodyPart = value;
+    });
+  }
+}
+
+class BodyPart {
+  final String name;
+
+  const BodyPart._(this.name);
+
+  static const head = BodyPart._("Head");
+  static const torso = BodyPart._("Torso");
+  static const legs = BodyPart._("Legs");
+
+  @override
+  String toString() {
+    return 'BodyPart{name: $name}';
+  }
+}
+
+class BodyPartButton extends StatelessWidget {
+  final BodyPart bodyPart;
+  final bool selected;
+  final ValueSetter<BodyPart> bodyPartSetter;
+
+  const BodyPartButton({
+    Key? key,
+    required this.bodyPart,
+    required this.selected,
+    required this.bodyPartSetter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => bodyPartSetter(bodyPart),
+      child: SizedBox(
+        height: 40,
+        child: ColoredBox(
+          color: selected
+              ? const Color.fromRGBO(28, 121, 206, 1)
+              : const Color.fromRGBO(0, 0, 0, 0.38),
+          child: Center(child: Text(bodyPart.name.toUpperCase())),
+        ),
+      ),
+    );
+  }
+}
